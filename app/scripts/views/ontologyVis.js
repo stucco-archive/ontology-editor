dd9be5e85512b3218cd24253466220dd56a0ba07
Lane Harrison <lanetharrison@gmail.com>
2013-06-30 22:18:01 +0000

    simple d3 showing nodes inferred from yaml, links to come

--- app/scripts/views/ontologyVis.js
+++ app/scripts/views/ontologyVis.js
@@ -12,14 +12,59 @@ define(
     'use strict';
 
     function ontologyVis() {
+      var w    = 660
+        , h    = 500
+        , vis;
+
+      var force = d3.layout.force()
+        .size([w, h]);
 
       function initView(el) {
-        d3.select(el).text('hiho');
+        vis = d3.select(el).append('svg')
+          .attr('width', w)
+          .attr('height', h);
+
+        vis.append('rect')
+          .attr('width', w)
+          .attr('height', h)
+          .attr('stroke', 'black')
+          .attr('fill', 'none');
       }
 
       function dumpYAML(evt, d) {
         var x = jsyaml.load(d.text);
-        this.$node.text(JSON.stringify(x));
+        updateVis(x);
+      }
+
+      function updateVis(d) {
+        //  coerce to objects
+        var nodes = Object.keys(d).map(function (d) {
+          return {name: d};
+        });
+
+        force.stop();
+
+        var node = vis.selectAll('.node')
+          .data(nodes)
+          .enter().append('circle')
+          .attr('class', 'node')
+          .attr('r', 5)
+          .attr('cx', 50)
+          .attr('cy', 50)
+          .style('fill', 'steelblue')
+          .call(force.drag);
+
+        node.append('title')
+          .text(function(d) { return d.name; });
+
+        force.on('tick', function() {
+          vis.selectAll('.node')
+            .attr('cx', function(d) { return d.x; })
+            .attr('cy', function(d) { return d.y; });
+        });
+
+        force.nodes(nodes)
+          .start();
       }
 
       this.after('initialize', function() {
