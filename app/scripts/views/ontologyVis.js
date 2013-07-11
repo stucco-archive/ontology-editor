c97f601e5d4d2ca2c39a6a20fe75f3cec99391e8
Lane Harrison <lanetharrison@gmail.com>
2013-07-10 21:47:35 -0400

    labels and updates

--- app/scripts/views/ontologyVis.js
+++ app/scripts/views/ontologyVis.js
@@ -43,24 +43,44 @@ define(
 
         force.stop();
 
+        // TODO this is inefficient. Comment it out and figure out how to update properly
+        vis.selectAll('.link').remove();
+        vis.selectAll('.node').remove();
+
+        force
+          .linkDistance(75)
+          .charge(-400)
+          .links(d.edges)
+          .nodes(d.vertices);
+
         var link = vis.selectAll('.link')
-          .data(d.edges)
+          .data(force.links())
           .enter().append('line')
           .attr('class', 'link')
           .style('stroke-width', 1)
           .style('stroke', 'black');
 
         var node = vis.selectAll('.node')
-          .data(d.vertices)
-          .enter().append('circle')
-          .attr('class', 'node')
-          .attr('r', 5)
-          .style('fill', 'steelblue')
-          .call(force.drag);
+          .data(force.nodes(), function(d) { return d.name; });
 
-        node.append('title')
-            .text(function(d) { return d._id; });
+        var g = node.enter().append('g')
+          .attr('class', 'node');
 
+          g.append('circle')
+            .attr('r', 5)
+            .style('fill', 'steelblue')
+            .call(force.drag)
+            .append('title')
+              .text(function(d) { return d._id; });
+
+          g.append("text")
+          .attr("class", "linktext")
+          .attr("x", 12)
+          .attr("dy", ".35em")
+          .text(function(d) { return d.name; });
+
+        node.exit().remove();
+        
         force.on('tick', function() {
           vis.selectAll('.link')
             .attr('x1', function(d) { return d.source.x; })
@@ -68,15 +88,16 @@ define(
             .attr('x2', function(d) { return d.target.x; })
             .attr('y2', function(d) { return d.target.y; });
 
-          vis.selectAll('.node')
+          vis.selectAll(".node .linktext")
+            .attr("dx", function(d) { return d.x; })
+            .attr("dy", function(d) { return d.y; });
+
+          vis.selectAll('.node circle')
             .attr('cx', function(d) { return d.x; })
             .attr('cy', function(d) { return d.y; });
         });
 
-        force
-          .links(d.edges)
-          .nodes(d.vertices)
-          .start();
+        force.start();
       }
 
       this.after('initialize', function() {
