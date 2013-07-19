331e0b9e96dc57b173abc1b93614ced1b1151273
Lane Harrison <lanetharrison@gmail.com>
2013-07-19 15:28:48 +0000

    added fullscreen button (to be replaced by keyboard eventually), fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,19 @@
 require.config({
   paths: {
       jquery:     '../bower_components/jquery/jquery',
+      es5shim:    '../bower_components/es5-shim/es5-shim',
       flight:     '../bower_components/flight/',
+      fullscreen: '../bower_components/flight-fullscreen/lib/fullscreen',
       d3:         '../bower_components/d3/d3',
       d3chart:    '../bower_components/d3.chart/d3.chart',
       bootstrap:  'vendor/bootstrap'
     },
     shim: {
+      'flight/lib/component': {
+        deps: ['jquery']
+      },
       bootstrap: {
-        deps: ['jquery'],
-        exports: 'jquery'
+        deps: ['jquery']
       },
       d3chart: {
         deps: ['d3']
@@ -17,8 +21,14 @@ require.config({
     }
   });
 
-require(['app', 'jquery'], function (app, $) {
+require(['app', 'jquery', 'flight/lib/component', 'fullscreen'], function (app, $, defineComponent, fs) {
   'use strict';
+
+  // TODO trigger via keyboard shortcut instead of button
+  fs.attachTo('#fs', {
+    toggleEvents: ['click'], // Events that toggle fullscreen.
+    target: 'body' // Specify different target.
+  });
 
   $().ready(function () {
     app.init();
