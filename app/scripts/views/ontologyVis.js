5a09710e3c14297a77e025019cd7eaffef2f59eb
Lane Harrison <lanetharrison@gmail.com>
2013-07-24 22:46:07 -0400

    Simplified node-link d3 code

--- app/scripts/views/ontologyVis.js
+++ app/scripts/views/ontologyVis.js
@@ -11,30 +11,28 @@ define(
     'use strict';
 
     function ontologyVis() {
-      var w
-        , h
-        , vis
+      var vis
         , color = d3.scale.category10()
         , force = d3.layout.force();
 
-      function initView(el, attrs) {
-        w = attrs.width;
-        h = attrs.height;
-
-        force
-          .size([w, h])
-          .linkDistance( d3.min([w, h]) / 2 )
-          .charge( -(w * 0.8) );
+      function init(el, attr) {
+        var w = attr.width
+          , h = attr.height;
 
         vis = d3.select(el).append('svg')
           .attr('width', w)
           .attr('height', h);
+
+        force.size([w, h])
+          .linkDistance( d3.min([w, h]) / 2 )
+          .charge( -(w * 0.8) );
       }
 
-      function updateVis(evt, x) {
+      function update(evt, x) {
         var d = JSON.parse(x.text)
           , r = 12;
 
+        // adjust edge attributes to work with d3.force
         d.edges.map(function (d) {
           d.source = parseInt(d._inV, 10);
           d.target = parseInt(d._outV, 10);
@@ -48,68 +46,70 @@ define(
         vis.selectAll('.link').remove();
         vis.selectAll('.node').remove();
 
-        var link = vis.selectAll('.link')
-          .data(force.links());
+        var links = vis.selectAll('.link')
+          .data( force.links() );
 
-        link.enter().append('g')
+        var linkGroup = links.enter().append('g')
           .attr('class', 'link');
 
-        link.append('line')
+        linkGroup.append('line')
           .style('stroke-width', 1)
           .style('stroke', 'black');
 
-        link.append('text')
+        linkGroup.append('text')
           .attr('class', 'linktext')
           .text(function(d) { return d._label; });
 
-        var node = vis.selectAll('.node')
+        links.exit().remove();
+
+        var nodes = vis.selectAll('.node')
           .data(force.nodes(), function(d) { return d.name; });
 
-        var g = node.enter().append('g')
+        var nodeGroup = nodes.enter().append('g')
           .attr('class', 'node');
 
-        g.append('circle')
+        nodeGroup.append('circle')
           .attr('r', r)
           .style('fill', function(d) { return color(d.group); })
-          .call(force.drag)
-            .append('title')
-            .text(function(d) { return d.group; });
+          .call(force.drag);
 
-        g.append('text')
+        nodeGroup.append('title')
+          .text(function(d) { return d.group; });
+        
+        nodeGroup.append('text')
           .attr('class', 'nodetext')
           .attr('x', r)
           .attr('dy', '.35em')
           .text(function(d) { return d.name; });
 
-        node.exit().remove();
+        nodes.exit().remove();
 
         force.on('tick', function() {
-          vis.selectAll('.link line')
+          linkGroup.selectAll('line')
             .attr('x1', function(d) { return d.source.x; })
             .attr('y1', function(d) { return d.source.y; })
             .attr('x2', function(d) { return d.target.x; })
             .attr('y2', function(d) { return d.target.y; });
 
-          vis.selectAll('.link .linktext')
+          linkGroup.selectAll('.linktext')
             .attr('dx', function(d) { return (d.source.x + d.target.x)/2; })
             .attr('dy', function(d) { return (d.source.y + d.target.y)/2; });
 
-          vis.selectAll('.node .nodetext')
+          nodeGroup.selectAll('.nodetext')
             .attr('dx', function(d) { return d.x; })
             .attr('dy', function(d) { return d.y; });
 
-          vis.selectAll('.node circle')
+          nodeGroup.selectAll('circle')
             .attr('cx', function(d) { return d.x; })
             .attr('cy', function(d) { return d.y; });
-
         });
 
         force.start();
       }
 
       this.after('initialize', function() {
-        initView(this.node, this.attr);
-        this.on('#ontologyText', 'textChange', updateVis);
+        init(this.node, this.attr);
+        this.on('#ontologyText', 'textChange', update);
       });
 
     }
