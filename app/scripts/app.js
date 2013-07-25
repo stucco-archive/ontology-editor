91fb1ca28dc5416c7e4af30d970dd608711743aa
Lane Harrison <lanetharrison@gmail.com>
2013-07-24 22:48:47 -0400

    Simplify initialization, adding behavior and keyboard shortcuts to their own functions.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,21 +11,6 @@ define(
   function(text, vis, defineComponent, fullscreen, kb) {
     'use strict';
 
-    kb.attachTo(document, {
-      shortcuts: {
-        f:    [ { eventName:  'fullscreenToggle' } ],
-        '/':  [ { eventName:  'search' } ],
-        e:    [ { eventName:  'focusEditor' } ],
-        u:    [ { eventName:  'undo' } ],
-        r:    [ { eventName:  'redo' } ]
-      }
-    });
-
-    fullscreen.attachTo(document, {
-      toggleEvents: ['fullscreenToggle'],
-      target: 'body'
-    });
-
     function updateSize () {
       return {
         width: $(window).width() / 2 - 50
@@ -33,16 +18,38 @@ define(
       };
     }
 
-    function init() {
-      var size = updateSize();
+    function keyboard() {
+      var shortcuts = {
+        f:    [ { eventName:  'fullscreenToggle' } ],
+        '/':  [ { eventName:  'search' } ],
+        e:    [ { eventName:  'focusEditor' } ],
+        u:    [ { eventName:  'undo' } ],
+        r:    [ { eventName:  'redo' } ]
+      };
+      kb.attachTo(document, { shortcuts: shortcuts });
+    }
 
-      $(window).resize(function() {
-        size = updateSize();
-        // TODO - call text and vis to resize
+    function events() {
+      fullscreen.attachTo(document, {
+        toggleEvents: ['fullscreenToggle'],
+        target: 'body'
       });
 
-      text.attachTo('#ontologyText', {width: size.width, height: size.height});
-      vis.attachTo('#ontologyVis', {width: size.width, height: size.height});
+      $(window).resize(function() {
+        // TODO call text and vis to resize
+        // var size = updateSize();
+      });
+    }
+
+    function init() {
+      var size  = updateSize()
+        , attrs = { width: size.width, height: size.height };
+
+      text.attachTo('#ontologyText', attrs);
+      vis.attachTo('#ontologyVis', attrs);
+
+      keyboard();
+      events();
     }
 
     return {
