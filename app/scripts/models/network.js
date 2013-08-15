032120f17e527a5fd49f73325f1b16888f48a7c2
Lane Harrison <lanetharrison@gmail.com>
2013-08-15 14:05:29 +0000

    minor simplification

--- app/scripts/models/network.js
+++ app/scripts/models/network.js
@@ -16,19 +16,6 @@ define(
         this.on(document, 'textChange', update);
       });
 
-      function adjustNetwork(d) {
-        d.vertices = d.properties.vertices.items;
-        d.edges    = d.properties.edges.items;
-
-        d.edges = d.edges.map(function(d) {
-          d.inV = d.properties.inVType.enum[0];
-          d.outV = d.properties.outVType.enum[0];
-          return d;
-        });
-
-        return d;
-      }
-
       // Each update, we have two sets of nodes/links: 
       //   nodes/links (old) and d.edges/d.vertices (new).
       //
@@ -41,32 +28,24 @@ define(
       // Also see modifying a force layout: http://bl.ocks.org/mbostock/1095795
       // tl;dr: d3.force responds to push events. Only add nodes if they're new.
       function update(evt, d) {
+        var prefix = 'gov.ornl.sava.stucco/graph/vertices/';
+
         d = JSON.parse(d.text);
 
-        // TODO copy edges and vertices into the format I need/want. Adjust as needed.
-        // TODO (this means identifying all the properties I used:
-        // 
-        // old, new, want
-        // _id (int), id (string), id (?)
-        // target, properties.inVType.enum[0], target
-        // source, properties.outVType.enum[0], source
-        // _label, title, title
-        // name, title, title
-        // group, null, null
-        //
-        // Vertices/Edges now have .description, this would be good to show.
-        //
-
-        d = adjustNetwork(d);
-
-        var prefix = 'gov.ornl.sava.stucco/graph/vertices/';
+        // adjust incoming schema to something that is more manageable
+        d.vertices = d.properties.vertices.items;
+        d.edges    = d.properties.edges.items;
+        d.edges = d.edges.map(function(o) {
+          o.inV  = o.properties.inVType.enum[0];
+          o.outV = o.properties.outVType.enum[0];
+          return o;
+        });
 
         // merge nodes
         var allNodes = _.uniq( _.union(nodes, d.vertices), function(d) {
           return d.id;
         });
         _.each( allNodes, function(o) {
-          // TODO simplify/functionify the filter part
           var inNew = _.filter(d.vertices, function(d) { return d.id === o.id; })[0]
             , inOld = _.filter(nodes, function(d) { return d.id === o.id; })[0];
 
