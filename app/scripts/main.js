b1a4239648b859b5bbf2c4acfa1206c1414f2469
John Goodall <jgoodall@gmail.com>
2014-04-15 15:29:17 -0400

    using min version of js libs, no bootstrap, export d3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,25 @@
 require.config({
   paths: {
-      jquery:     '../bower_components/jquery/jquery',
-      es5shim:    '../bower_components/es5-shim/es5-shim',
-      es5sham:    '../bower_components/es5-shim/es5-sham',
+      jquery:     '../bower_components/jquery/jquery.min',
+      es5shim:    '../bower_components/es5-shim/es5-shim.min',
+      es5sham:    '../bower_components/es5-shim/es5-sham.min',
       flight:     '../bower_components/flight/',
       fullscreen: '../bower_components/flight-fullscreen/lib/fullscreen',
       kb:         '../bower_components/flight-keyboard-shortcuts/lib/keyboard-shortcuts',
-      d3:         '../bower_components/d3/d3',
+      d3:         '../bower_components/d3/d3.min',
       d3chart:    '../bower_components/d3.chart/d3.chart',
-      underscore: '../bower_components/underscore/underscore',
-      bootstrap:  'vendor/bootstrap'
+      underscore: '../bower_components/underscore/underscore-min',
+      // bootstrap:  'vendor/bootstrap'
     },
     shim: {
       'flight/lib/component': {
         deps: ['jquery', 'es5shim', 'es5sham']
       },
-      bootstrap: {
-        deps: ['jquery']
+      // bootstrap: {
+      //   deps: ['jquery']
+      // },
+      d3: {
+        exports: 'd3'
       },
       d3chart: {
         deps: ['d3']
